3b4173aff82aa541c529268b43e310552c82c2f6
DalanDan <jonathanroi.agarrado@my.jcu.edu.au>
2023-07-24 14:51:59 +0800

    Complete 31

--- Sections/2/31 (Magic methods; __str__ and __repr__)/code.py
+++ Sections/2/31 (Magic methods; __str__ and __repr__)/code.py
@@ -3,9 +3,12 @@ class Person:
         self.name = name
         self.age = age
 
-    def __str__(self):
-        return f"Person {self.name}, {self.age} years old."
+    # def __str__(self):
+    #     return f"Person {self.name}, {self.age} years old."
     
+    def __repr__ (self):
+        return f"<Person('{self.name}', {self.age})>"
+
 
 bob = Person("Bob", 35)
 print(bob)
