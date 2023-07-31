dd081420369c049988f025d7034974cdb9b94609
tigerdalandan@gmail.com <118592614+JonathanAgarradoJCU@users.noreply.github.com>
2023-08-01 01:24:35 +0800

    Continue 38

--- Sections/2/38/code.py
+++ Sections/2/38/code.py
@@ -1,11 +1,15 @@
 def divide(dividend, divisor):
     if divisor == 0:
-        print("Divisor cannot be 0.")
-        return
+        raise ZeroDivisionError("Divisor cannot be 0.")
     
     return dividend / divisor
 
 
 divide(15, 2)
 
+grades = [97, 85, 87, 66, 88]
+
+average = divide(sum(grades), len(grades))
+
+print(f"Your average grade is {average}.")
 # print(divide(15, 2))
