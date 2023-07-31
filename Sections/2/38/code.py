568a12185654fcc6552d5dd8fd7385adbfca659a
tigerdalandan@gmail.com <118592614+JonathanAgarradoJCU@users.noreply.github.com>
2023-07-31 23:58:31 +0800

    Attempt 38

---
+++ Sections/2/38/code.py
@@ -0,0 +1,11 @@
+def divide(dividend, divisor):
+    if divisor == 0:
+        print("Divisor cannot be 0.")
+        return
+    
+    return dividend / divisor
+
+
+divide(15, 2)
+
+# print(divide(15, 2))
